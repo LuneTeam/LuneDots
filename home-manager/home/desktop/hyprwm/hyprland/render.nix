fe3018488e93b21f8980d2e4ea9f4dfc6f169fdc
LaiMicK <hyprland@vk.com>
2025-06-21 18:52:23 +0500

    hyprwm: explicit_sync settings in render: have been nuked

--- home-manager/home/desktop/hyprwm/hyprland/render.nix
+++ home-manager/home/desktop/hyprwm/hyprland/render.nix
@@ -1,10 +1,6 @@
 { ... }:
 {
   wayland.windowManager.hyprland.settings = {
-    render = {
-      explicit_sync = 1;
-      explicit_sync_kms = 1;
-    };
     xwayland = {
       enabled = false;
     };
