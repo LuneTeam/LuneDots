20dfbd09039667697607de59973c8ab8760988f4
LaiMicK <hyprland@vk.com>
2025-07-06 01:45:05 +0500

    nixvim: new colorschemes added

--- home-manager/home/development/nixvim/modules/colorscheme/default.nix
+++ home-manager/home/development/nixvim/modules/colorscheme/default.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
   imports = [
-    ./tokyonight.nix
+    ./oxocarbon.nix
   ];
 }
--- home-manager/home/development/nixvim/modules/colorscheme/kanagawa.nix
+++ home-manager/home/development/nixvim/modules/colorscheme/kanagawa.nix
@@ -2,5 +2,6 @@
 {
   programs.nixvim = {
     colorschemes.kanagawa.enable = true;
+    colorschemes.kanagawa.settings.theme = "dragon";
   };
 }
