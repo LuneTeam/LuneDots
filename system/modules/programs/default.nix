cdd97c475750e47c6f611d0ed809d48604c49b7b
LaiMicK <hyprland@vk.com>
2025-07-16 10:24:41 +0500

    pkgs: home-manager fonts -> global fonts

--- system/modules/programs/default.nix
+++ system/modules/programs/default.nix
@@ -1,9 +1,41 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs = {
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-    };
+  fonts = {
+    packages = with pkgs; [
+      # UI & General Purpose Fonts
+      liberation_ttf
+      dejavu_fonts
+      roboto
+      libertinus
+      (google-fonts.override { fonts = [ "Inter" ]; })
+
+      # Monospace & Programming Fonts
+      jetbrains-mono
+      fira-code
+      fantasque-sans-mono
+      cascadia-code
+      maple-mono.NF
+
+      # Nerd Fonts
+      nerd-fonts.symbols-only
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.caskaydia-cove
+      nerd-fonts.hack
+      nerd-fonts.iosevka
+      nerd-fonts.fira-code
+      nerd-fonts.fira-mono
+      nerd-fonts.dejavu-sans-mono
+      nerd-fonts.space-mono
+
+      # Emoji & Symbol Fonts
+      noto-fonts-emoji
+      twemoji-color-font
+
+      # CJK (Asian Language)
+      noto-fonts-cjk-sans
+
+      # Icon Fonts
+      material-symbols
+    ];
   };
 }
